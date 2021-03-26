6d33db2ca30ae38999a0a351994ace01b499df8e
Calvin Williams <calvin.william1012@gmail.com>
2021-03-26 01:53:30 -0400

    added arrow icons and hid section content

--- components/section.js
+++ components/section.js
@@ -2,65 +2,72 @@ import Link from "next/link";
 
 export default function Section({ sectionData }) {
   return (
-    <section className="hd-border">
-      <h1>{sectionData.title}</h1>
+    <section className="hd-border main-section">
       <div>
-        {/* mailto contact link */}
-        {sectionData.email ? (
-          <a href={"mailto:" + sectionData.email}>{sectionData.email}</a>
-        ) : null}
-        {/* link to my github */}
-        {sectionData.github ? (
-          <a href={sectionData.github}>See my GitHub</a>
-        ) : null}
-        {/* print out all my technologies*/}
-        {sectionData.tech ? (
-          <>
-            <h3>Tech</h3>
-            <h4>Proficient:</h4>
-            <ul>
-              {sectionData.tech.proficient.map((t) => (
-                <li key={t}>{t}</li>
-              ))}
-            </ul>
-            <h4>Familiarity</h4>
-            <ul>
-              {sectionData.tech.familiarity.map((t) => (
-                <li key={t}>{t}</li>
-              ))}
-            </ul>
-          </>
-        ) : null}
-        {/* print job data conditionally, only supports 1 job
+        <div>
+          <i className="icon icon-chevron-down"></i>
+          <h1>{sectionData.title}</h1>
+        </div>
+        <div className="section-content">
+          {/* mailto contact link */}
+          {sectionData.email ? (
+            <a href={"mailto:" + sectionData.email}>{sectionData.email}</a>
+          ) : null}
+          {/* link to my github */}
+          {sectionData.github ? (
+            <a href={sectionData.github}>See my GitHub</a>
+          ) : null}
+          {/* print out all my technologies*/}
+          {sectionData.tech ? (
+            <>
+              <h3>Tech</h3>
+              <h4>Proficient:</h4>
+              <ul>
+                {sectionData.tech.proficient.map((t) => (
+                  <li key={t}>{t}</li>
+                ))}
+              </ul>
+              <h4>Familiarity</h4>
+              <ul>
+                {sectionData.tech.familiarity.map((t) => (
+                  <li key={t}>{t}</li>
+                ))}
+              </ul>
+            </>
+          ) : null}
+          {/* print job data conditionally, only supports 1 job
       need to change md and below section to use map for more than one*/}
-        {sectionData.jobs ? (
-          <>
-            <h3>Work</h3>
-            <h4>
-              {sectionData.jobs.company} - {sectionData.jobs.title}
-            </h4>
-            <span>
-              {sectionData.jobs.startDate} -{" "}
-              {sectionData.jobs.endDate ? sectionData.jobs.endDate : "current"}
-            </span>
-          </>
-        ) : null}
-        {/* print education data conditionally, only supports 1 education
+          {sectionData.jobs ? (
+            <>
+              <h3>Work</h3>
+              <h4>
+                {sectionData.jobs.company} - {sectionData.jobs.title}
+              </h4>
+              <span>
+                {sectionData.jobs.startDate} -{" "}
+                {sectionData.jobs.endDate
+                  ? sectionData.jobs.endDate
+                  : "current"}
+              </span>
+            </>
+          ) : null}
+          {/* print education data conditionally, only supports 1 education
       need to change md and below section to use map for more than one*/}
-        {sectionData.education ? (
-          <>
-            <h3>Education</h3>
-            <h4>
-              {sectionData.education.school} - {sectionData.education.title}
-            </h4>
-            <span>
-              {sectionData.education.startDate} -{" "}
-              {sectionData.education.endDate
-                ? sectionData.education.endDate
-                : "current"}
-            </span>
-          </>
-        ) : null}
+          {sectionData.education ? (
+            <>
+              <h3>Education</h3>
+              <h4>
+                {sectionData.education.school} - {sectionData.education.title}
+              </h4>
+              <span>
+                {sectionData.education.startDate} -{" "}
+                {sectionData.education.endDate
+                  ? sectionData.education.endDate
+                  : "current"}
+              </span>
+            </>
+          ) : null}
+        </div>
       </div>
     </section>
   );
--- data/Contact.md
+++ data/Contact.md
@@ -1,7 +1,7 @@
 ---
 order: 1
 title: 'Contact'
-email: 'mailto:calvin@calvinwilliams.ca'
+email: 'calvin@calvinwilliams.ca'
 linkedin: 'https://www.linkedin.com/in/calvin-williams-bb1396137/'
 ---
 
